0d21c2e420047be29f186db3d51f73f6da3df411
Kevin van Sonsbeek <99041317@mydavinci.nl>
2018-04-10 09:16:06 +0200

    Made pokemon class, and 2 extending classes Pikachu and Charmeleon

---
+++ Pikachu.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Class Pikachu
+ */
+class Pikachu extends Pokemon
+{
+
+
+
+    public function __construct($name = "Pikachu")
+    {
+        $type = "Electric";
+        $hitpoints = 60;
+        $weaknesses = array(
+            new Weakness("Fire", 1.5),
+        );
+        $resistances = array(
+            new Resistance("Fighting", 20),
+        );
+        $attacks = array(
+            new Attack("Electric Ring", 50),
+            new Attack("Pika Punch", 20)
+        );
+        parent::__construct($type, $name, $hitpoints, $weaknesses, $resistances, $attacks);
+    }
+}
--- Pokemon.php
+++ Pokemon.php
@@ -3,21 +3,85 @@
 /**
  * Class Pokemon
  */
-class Pokemon {
-    protected $name;
+class Pokemon
+{
+
+    public $name;
     public $energyType;
     public $hitPoints;
-    public $attacks;
+    protected $hp;
     public $weakness;
-    public $ressitance;
+    public $resistance;
+    public $attacks;
 
 
-    public function __construct($name){
+    public function __construct($type, $name, $hitpoints, $weakness, $resistance, $attacks)
+    {
+        $this->energyType = $type;
         $this->name = $name;
+        $this->hitPoints = $hitpoints;
+        $this->hp = $hitpoints;
+        $this->weakness = $weakness;
+        $this->resistance = $resistance;
+        $this->attacks = $attacks;
     }
 
-    public function attack() {
-
+    /**
+     * Attack pokemon with move
+    */
+    public function attack($target, $attack)
+    {
+        echo "<div style='border: 1px solid black'>";
+        echo "<p><b>" . $this->name . "</b> attacked <b>" . $target->name . "</b> with the move <b>" . $attack->name . "</b></p>";
+        $target->defend($this, $attack);
     }
+
+    /**
+     * Defend pokemon
+     */
+    public function defend($attacker, $attack) {
+
+        $total_damage = $attack->damage;
+
+        //Loop through weaknesses
+        foreach ($this->weakness as $weaknes) {
+            if ($attacker->energyType == $weaknes->energy_type) {
+                $total_damage = $total_damage * $weaknes->value;
+            }
+        }
+
+        //Loop through resistances
+        foreach ($this->resistance as $resistance) {
+            if($attacker->energyType == $resistance->energy_type) {
+                print_r($resistance);
+                $total_damage = $total_damage - $resistance->value;
+            }
+        }
+
+
+        echo "<p> It dealt " . $total_damage . " damage!</p>";
+
+        $this->take_damage($total_damage);
+    }
+
+    /**
+    * Make pokemon take damage
+    * @param $amount
+    */
+    protected function take_damage($amount) {
+
+        $this->hp = $this->hp - $amount;
+        echo "<p>" . $this->name . " has " . $this->get_hitpoints() . " hp left from the " . $this->hitPoints . "</p>";
+        echo "</div>";
+    }
+
+    /**
+    * Get pokemons HP
+    */
+    public function get_hitpoints() {
+        return $this->hp;
+    }
+
+
 
 }
